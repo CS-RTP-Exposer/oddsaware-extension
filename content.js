bb7a94ba947fafadce01fdf86fe4ae25a4adfa81
me-ch1cken <rdecroo24@gmail.com>
2025-05-19 18:47:46 +0200

    added tracker to check if total of odds is 100% (since csgoroll has breaking bug in fe)

--- content.js
+++ content.js
@@ -11,22 +11,47 @@ document.addEventListener('click', async (event) => {
     await sleep(1000); // Wacht 1 seconde om te zorgen dat de DOM is geladen
 
     const wrapper = document.querySelector('.box-details-side-caption');
-    const cost = wrapper.querySelector('.currency-value').innerText;
+    const cost = parseFloat(wrapper.querySelector('.currency-value').innerText);
     const items = wrapper.querySelectorAll('.w-50');
 
     let totalRtp = 0;
+    let totalPercentage = 0;
+
+    console.log('Cost:', cost);
 
     items.forEach(item => {
 
       const itemValues = item.innerText.split('\n');
-      const percentage = itemValues[0].substring(0, itemValues[0].length - 1) / 100;
-      const value = itemValues[itemValues.length - 1].replace(',', '');
+      const percentage = parseFloat(itemValues[0].substring(0, itemValues[0].length - 1)) / 100;
+      const value = parseFloat(itemValues[itemValues.length - 1].replace(',', ''));
 
+      console.log('Percentage:', percentage);
+      console.log('Value:', value);
+
+      totalPercentage += percentage;
       totalRtp += value * percentage;
     });
 
     const rtp = (totalRtp / cost) * 100;
     console.log('RTP:', rtp);
+    console.log('Total percentage:', totalPercentage);
+
+    const siblingElement = wrapper.querySelector('img.align-self-center');
+    
+    const existingRtp = wrapper.querySelector('.custom-rtp');
+    if (existingRtp) existingRtp.remove();
+
+    // Maak RTP-weergave element
+    const rtpElement = document.createElement('div');
+    rtpElement.className = 'custom-rtp';
+    rtpElement.style.marginTop = '8px';
+    rtpElement.style.fontWeight = 'bold';
+    rtpElement.style.color = totalPercentage !== 1 ? 'red' : rtp >= 100 ? 'green' : rtp >= 80 ? 'orange' : 'red';
+    rtpElement.innerText = totalPercentage !== 1 ? `Total percentage is not 100%` : `RTP: ${rtp.toFixed(2)}%`;
+
+    // Voeg het RTP-element toe onder de afbeelding
+    siblingElement.insertAdjacentElement('afterend', rtpElement);
+
 
   }
 });
