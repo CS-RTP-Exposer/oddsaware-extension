f3156a064348bba7d75d217f240c49ed647ea3cb
me-ch1cken <rdecroo24@gmail.com>
2025-05-21 16:06:13 +0200

    implemented solo openings for rain.gg

--- rain_solo_content.js
+++ rain_solo_content.js
@@ -1,5 +1,9 @@
 let lastUrl = location.href;
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function removeElements() {
     let targetDiv = document.querySelector('main > div > div.sc-f63de73e-0.dNqLFf');
 
@@ -21,7 +25,85 @@ async function removeElements() {
         childList: true
     });
 }
-removeElements();
+
+async function calculateRTP() {
+    let wrapper = document.querySelector('div.sc-f63de73e-0.bILPKE');
+    let cost = null;
+    let items = null;
+
+    while (wrapper === null) {
+        wrapper = document.querySelector('div.sc-f63de73e-0.bILPKE');
+        await sleep(10);
+    }
+
+    while (cost === null || items === null || items.length === 0) {
+        cost = document.querySelector('div.sc-b69e8a97-0.cKiNwf');
+        items = wrapper.querySelectorAll('div.sc-f63de73e-0.bILPKE > div.sc-78e8d11f-0');
+        await sleep(10);
+    }
+
+    cost = parseFloat(cost.innerText);
+    
+    let totalRtp = 0;
+    let totalPercentage = 0;
+    let profitPercentage = 0;
+    let avgReturn = 0;
+    let profitItems = [];
+
+    items.forEach(item => {
+        const itemValues = item.innerText.split('\n');
+        const percentage = parseFloat(itemValues[0].substring(0, itemValues[0].length - 1)) / 100;
+        const value = parseFloat(itemValues[itemValues.length - 1].replace(',', ''));
+
+        totalPercentage += percentage;
+        totalRtp += value * percentage;
+
+        if (value >= cost) {
+            profitPercentage += percentage;
+            profitItems.push({ value, percentage });
+        }
+    });
+
+    profitItems.forEach(item => {
+        avgReturn += item.value * ((item.percentage * 100 / profitPercentage) / 100);
+    });
+
+    const rtp = (totalRtp / cost) * 100;
+    const avgProfit = (avgReturn - cost);
+        totalPercentage = Math.round(totalPercentage);
+
+        const siblingElement = document.querySelector('div.sc-853883c2-0.fsxdNL');
+        const existingRtp = wrapper.querySelector('.custom-rtp');
+        const existingProfit = wrapper.querySelector('.profit');
+
+        if (existingRtp) existingRtp.remove();
+        if (existingProfit) existingProfit.remove();
+
+        const rtpElement = document.createElement('div');
+        rtpElement.className = 'custom-rtp';
+        rtpElement.style.marginTop = '8px';
+        rtpElement.style.fontWeight = 'bold';
+        rtpElement.style.color = totalPercentage !== 1 ? 'red' : rtp >= 100 ? 'green' : rtp >= 80 ? 'orange' : 'red';
+        rtpElement.innerText = totalPercentage !== 1 ? `Total percentage is not 100%` : `RTP: ${rtp.toFixed(2)}%`;
+
+        const profitElement = document.createElement('div');
+        profitElement.className = 'profit';
+        profitElement.style.marginBottom = '16px';
+        profitElement.style.fontWeight = 'bold';
+        profitElement.style.color = profitPercentage >= 0.5 ? 'green' : 'red';
+        profitElement.innerText = `Chance at profit: ${(profitPercentage * 100).toFixed(2)}% (avg. profit of ${(avgProfit / cost).toFixed(2)}x)`;
+
+        siblingElement.insertAdjacentElement('afterend', profitElement);
+        siblingElement.insertAdjacentElement('afterend', rtpElement);
+}
+
+if (window.location.href === 'https://rain.gg/games/case-opening') {
+    removeElements();
+}
+
+if (window.location.href.startsWith('https://rain.gg/games/case-opening/')) {
+    calculateRTP();
+}
 
 new MutationObserver(() => {
     const currentUrl = location.href;
@@ -32,7 +114,7 @@ new MutationObserver(() => {
 }).observe(document, { subtree: true, childList: true });
 
 function onUrlChange(url) {
-    if (url === 'https://rain.gg/games/case-opening') {
-        removeElements();
-    }
+    if (url === 'https://rain.gg/games/case-opening') removeElements();
+    if (url.startsWith('https://rain.gg/games/case-opening/')) calculateRTP();
+
 }
